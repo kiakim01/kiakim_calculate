7e541290dc608c118525785cc2a8017ea75079cd
kiakim01 <hnmpot0318@naver.com>
2023-07-18 20:28:03 +0900

    Initial Commit

---
+++ kiakim_calculateUITests/kiakim_calculateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  kiakim_calculateUITestsLaunchTests.swift
+//  kiakim_calculateUITests
+//
+//  Created by kiakim on 2023/07/18.
+//
+
+import XCTest
+
+final class kiakim_calculateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
